1207bda9d522a0d3ef2e862956fae117a519bb6f
Chuck <chuck.lindblom@gmail.com>
2020-09-01 21:47:11 -0400

    fixed issues
    - navbar
    - footer
    - featured products

---
+++ js/featured-product.js
@@ -0,0 +1,6 @@
+$(function() {
+    $("#fp1").load("/includes/featured_product_1.html");
+    $("#fp2").load("/includes/featured_product_2.html");
+    $("#fp3").load("/includes/featured_product_3.html");
+    $("#fp4").load("/includes/featured_product_4.html");
+});
--- js/main.js
+++ js/main.js
@@ -199,3 +199,18 @@ jQuery(document).ready(function($) {
 
 
 });
+
+$(document).ready(function(){
+	$('.slick_photo').slick({
+	  infinite: true,
+	  slidesToShow: 1,
+	  slidesToScroll: 1,
+	  autoplay: true,
+	  autoplaySpeed: 4000,
+	  dots: true,
+	  infinite: true,
+	  speed: 500,
+	  fade: true,
+	  cssEase: 'linear'
+	});
+  });

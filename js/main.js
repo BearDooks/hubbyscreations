45a23dc538f0a59144d99774d8e2a0ed03dd1a3c
Chuck <chuck.lindblom@gmail.com>
2020-09-01 22:51:02 -0400

    Remove slick add gallery

--- js/main.js
+++ js/main.js
@@ -199,18 +199,3 @@ jQuery(document).ready(function($) {
 
 
 });
-
-$(document).ready(function(){
-	$('.slick_photo').slick({
-	  infinite: true,
-	  slidesToShow: 1,
-	  slidesToScroll: 1,
-	  autoplay: true,
-	  autoplaySpeed: 4000,
-	  dots: true,
-	  infinite: true,
-	  speed: 500,
-	  fade: true,
-	  cssEase: 'linear'
-	});
-  });
